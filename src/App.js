86292df2506542cc29650034c5fea6c3a0cc61f6
Corey Lantz <clantz@ecentricarts.com>
2019-05-29 11:01:14 -0400

    adding scss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import Button from './components/button';
-import './App.css';
+import './App.scss';
 
 
 const App = () => {
